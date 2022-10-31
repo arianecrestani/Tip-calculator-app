8ecb965623a33ba4263ba2f5923b22fb27015c95
Ari <ariane.mrv@gmail.com>
2022-10-31 18:52:37 +0100

    first setup

--- src/Component/TipCalculator.jsx
+++ src/Component/TipCalculator.jsx
@@ -15,7 +15,30 @@ export default function TipCalculator() {
             <div className="tip-15">15%</div>
             <div className="tip-25">25%</div>
             <div className="tip-50">50%</div>
-            <div id ='tip-custom'><input className="tip-custom" placeholder="CUSTOM"/></div>
+            <div id="tip-custom">
+              <input className="tip-custom" placeholder="CUSTOM" />
+            </div>
+          </div>
+          <div className="secondSection">
+            <label>Number of People</label>
+            <label className="error">Can't be zero</label>
+          </div>
+          <input />
+        </div>
+        <div className="result">
+          <div className="tip-amount">
+            <div className="text">
+              <p>Tip Amount</p>
+              <p className="person">/person</p>
+            </div>
+            <div className="amount">$4.27</div>
+          </div>
+          <div className="total">
+            <div className="text">
+              <p>Tip Amount</p>
+              <p className="person">/person</p>
+            </div>
+            <div className="amount">$4.27</div>
           </div>
         </div>
       </div>
